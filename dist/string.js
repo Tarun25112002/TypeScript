05440a6fb1c7d558d91c47790954c10657af5864
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2026-01-03 22:42:05 +0530

    string and boolean done

---
+++ dist/string.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = `Tarun Kumar Jha`;
+let age = 23;
+let user = `User name is ${name}, and user age is ${age}.`;
+console.log(user);
+// boolean
+let userExist = true;
+console.log(userExist);
+//# sourceMappingURL=string.js.map
---
+++ src/string.ts
@@ -0,0 +1,8 @@
+let name: string = `Tarun Kumar Jha`;
+let age:number = 23;
+let user = `User name is ${name}, and user age is ${age}.`
+console.log(user);
+
+// boolean
+let userExist: boolean = true
+console.log(userExist);
