cbe4bd5cd9ffc1f6b2176e9a0821ccfcc8b54d37
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2026-01-04 11:47:50 +0530

    array done

---
+++ dist/array.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let users = ["Tarun", "Tanvi", "Sunita", "Dhirendra Jha"];
+for (let user of users) {
+    if (user == "Tarun") {
+        console.log(`user name is ${user}`);
+    }
+}
+//# sourceMappingURL=array.js.map
---
+++ src/array.ts
@@ -0,0 +1,7 @@
+let users: string[]= ["Tarun", "Tanvi", "Sunita", "Dhirendra Jha"]
+for(let user of users){
+    if(user=="Tarun"){
+        console.log(`user name is ${user}`);
+        
+    }
+}
