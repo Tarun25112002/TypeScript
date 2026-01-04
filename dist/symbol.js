1d28131ccf57bdc9210db040a3e6ea58194e908e
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2026-01-04 11:24:08 +0530

    add symbol handling; update start script and include source maps

---
+++ dist/symbol.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let sym = Symbol("Tarun");
+let sym2 = Symbol("Tarun");
+console.log(sym, sym2);
+//# sourceMappingURL=symbol.js.map
---
+++ src/symbol.ts
@@ -0,0 +1,3 @@
+let sym :symbol = Symbol("Tarun");
+let sym2 : symbol = Symbol("Tarun")
+console.log(sym, sym2)
